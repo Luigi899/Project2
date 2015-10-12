2b7de04f9b1e0d58dde458eafd08820175fc7bab
Luigi899 <ldantonio@csumb.edu>
2015-10-12 11:08:25 -0700

    Almost done

--- Project2.py
+++ Project2.py
@@ -1,21 +1,19 @@
-    '''
+'''
     STEGANOGRAPHY - Hiding messages in different types of media.
+    This part of the program is the encoder.
+    It will encode our message into the image.
     '''
 
-'''
-This part of the program is the encoder.
-It will encode our message into the image.
-'''
-
-#Required module: PIL
+#Required: Libraries
 from PIL import Image
+import getpass
 
-#Definition to encode message into image.
+#Definition to encode message into image
 def encode_message(img, msg):
     #Check message length & mode
-    limit = len(msg)
+    length = len(msg)
     
-    if(limit>255):
+    if(length>255):
         print("The message is too long. Keep it under 255 characters.")
         return False
     if(img.mode != "RGB"):
@@ -31,9 +29,10 @@ def encode_message(img, msg):
         for y in range(width):
             #Variables holding pixel values
             r, g, b = img.getpixel((y,x))
-            if x == 0 and y == 0 and counter < limit:
-                ascii_code = limit
-            elif counter <= limit:
+            if x == 0 and y == 0 and counter < length:
+                #First pixel will show message length
+                ascii_code = length
+            elif counter <= length:
                 char = msg[counter-1]
                 #Getting ASCII value
                 ascii_code = ord(char)
@@ -52,11 +51,33 @@ ori_img = "secret.bmp"
 img = Image.open(ori_img)
 enc_img = "enc_" + ori_img
 
-#Message to encode into image
-msg = str(input("Set message: "))
+#Definition to decode message from image
+def decode_message(img):
+    width, height = img.size
+    counter = 0
+    msg = ''; #Empty variable to hold message
+    
+    for x in range(height):
+        for y in range(width):
+            #Get pixels, for this purpose red is the focus
+            try:
+                r, b, g = img.getpixel((y,x))
+            except:
+                r, b, g, a = img.getpixel((y,x))
+            
+            if x == 0 and y == 0:
+                #First red pixel has our message length
+                length = r
+            elif counter <= length:
+                #Change r(ASCII) value to a character and add it to msg
+                msg += chr(r)
+            counter += 1
+return msg
 
-#Set password
-mypassword = str(input('Set password : '))
+#Set message to encode into image
+msg = getpass.getpass("Set message: ")
+#Set password to get decoded message
+mypassword = getpass.getpass('Set password: ')
 
 img_encoded = encode_message(img,msg)
 if img_encoded:
@@ -69,14 +90,14 @@ if img_encoded:
     os.startfile(enc_img)
     
     #Password Feature
-    password =  input('Enter password : ')
+    password = getpass.getpass("Enter password: ")
     
-    while(password != mypassword):
-        print("Wrong password! ")
-        print("Try again. ")
-        password =  input('Enter password : ')
+    while(password!=mypassword):
+        print("Wrong password!\nTry again.")
+        password = getpass.getpass("Enter password: ")
     
-    if(password == mypassword):
-        print("Secret Message : " + msg)
-              
-
+    if(password==mypassword):
+        img2 = Image.open(enc_img)
+        secret_msg = decode_message(img2)
+        print("Secret Message: " + secret_msg)
+        input("Press enter to exit.")
